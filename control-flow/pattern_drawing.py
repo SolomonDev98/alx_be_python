ef0f26e9f824162eebfbe74b54546411d23c0502
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-07-16 00:23:04 +0100

    Add a count variable and used while loop for the outer loop

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -4,10 +4,12 @@
 # Prompt User for Pattern size
 
 size = int(input("Enter the size of the pattern: "))
+count = 0
 
 # outer loop
-for i in range(size):
+while (size != count):
     # inner loop
     for j in range(size):
         print("*", end="")
+    count += 1
     print()
