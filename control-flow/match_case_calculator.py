40cafe95f860472a915df4659384fe0bb8ed473c
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-07-15 23:21:44 +0100

    Add a simple calculator program

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Simple Calculator with Match Case
+
+# Collect operands from user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# collect operator from user
+operator = input("Choose the operation(+, -, *, /): ")
+
+# Perform the Calculation Using Match Case:
+
+match operator:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case "/":
+        if num2 == 0:
+            print("Error: Zero Divison!.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
