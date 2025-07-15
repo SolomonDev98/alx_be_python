150a6319765513593cdbda6a1fb274dc66d7b673
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-07-15 22:51:31 +0100

    Add Weather Recommendation Program

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Weather Recommendation Program
+
+# Get input from user
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
+
+# Provide Clothing Recommendatios
+if (weather == "sunny"):
+    print("Wear a t-shirt and sunglasses.")
+elif (weather == "rainy"):
+    print("Don't forget your umbrella and a raincoat.")
+elif (weather == "cold"):
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
