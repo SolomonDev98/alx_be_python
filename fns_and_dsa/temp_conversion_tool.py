f753dc3cda88890870f43799e6e180c2469ba8cb
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-07-29 22:59:15 +0100

    fix: adjust the position of the global variables

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,8 +2,8 @@
 # temp_conversion_tool.py
 """Objective: Illustrate the concept of variable scope by creating a script that converts temperatures between Celsius and Fahrenheit, using global variables to store conversion factors.
 """
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
 # Function that converts fahrenheit to celsuis
 def convert_to_celsius(fahrenheit):
