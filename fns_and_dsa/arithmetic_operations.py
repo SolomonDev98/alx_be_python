16fe69e108f6abc62c44fed62567f33c9ce0c65b
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-07-29 20:21:19 +0100

    Add script that performs and returns the results for arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# arithmetic_operations.py
+"""Define a Function that performs basic arithmetic operations"""
+
+# function definition for arithmetic operations(add, subtract, multiply, divide)
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return "Error!, you denominator can not be 0"
+            else:
+                return num1 / num2
+        case _:
+            return "Invalid input, please try again"
