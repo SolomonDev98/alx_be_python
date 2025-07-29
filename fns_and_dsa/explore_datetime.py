1010a89b329be8fc0f9185adda7f7f939c27d3a1
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-07-29 22:04:55 +0100

    fix: Import the datetime.now() method and the timedelta class from the datetime module and not the entire datetime module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # explore_datetime.py
-import datetime
+from datetime import datetime, timedelta
 
 # Display current date and time
 def display_current_datetime():
-    return datetime.datetime.now()
+    return datetime.now()
 
 current_date = display_current_datetime()
 print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
@@ -13,7 +13,7 @@ print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
 number_of_days = int(input("Enter the number of days to add to the current date: "))
 
 def calculate_future_date():
-    return datetime.timedelta(days=number_of_days)
+    return timedelta(days=number_of_days)
 
 future_date = current_date + calculate_future_date()
 print(f"Future date:{future_date.strftime('%Y-%m-%d')}")
