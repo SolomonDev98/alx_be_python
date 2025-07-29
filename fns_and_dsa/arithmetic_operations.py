13829b66885fe8c0c41c8a7e321852fd302fd2de
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-07-29 20:25:16 +0100

    fix: Add an elif block to check for zero division error

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -14,7 +14,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error!, you denominator can not be 0"
-            else:
+            elif num2 != 0:
                 return num1 / num2
         case _:
             return "Invalid input, please try again"
