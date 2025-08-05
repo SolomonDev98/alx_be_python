3e0e1d3fe59be01d4139fc006564c7c3fc19fc9b
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-08-05 17:53:09 +0100

    Add script that test operations by a simple calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+    
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(-7, -3), -4)
+        self.assertEqual(self.calc.subtract(-5, 3), -8)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+        self.assertEqual(self.calc.multiply(-4, 4), -16)
+        self.assertEqual(self.calc.multiply(-4, -4), 16)
+        self.assertEqual(self.calc.multiply(0, 4), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(4, 10), 0.4)
+    
+    def test_divide_by_zero(self):
+        result = self.calc.divide(10, 0)
+        self.assertIsNone(result)
+
+if __name__ == "__main__":
+    unittest.main()
