844edac402a1dafc4056425a64e5578979f45a96
SolomonDev98 <solomonolungweonwi@gmail.com>
2025-08-05 17:09:37 +0100

    Add script that implements a division calc and handles errors

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# robust_division_calculator.py
+"""
+Objective: Implement a division calculator that robustly handles errors like division by zero and non-numeric inputs using command line arguments.
+"""
+
+def safe_divide(numerator, denominator):
+    # Function that performs divsion and handles zero_division_error
+    try:
+        result = int(numerator) / int(denominator)
+    except ZeroDivisionError as e:
+        return "Error: Cannot divide by zero."
+    except ValueError as e:
+        return "Error: Please enter numeric values only."
+    else:
+        return f"The result of the division is {result}"
